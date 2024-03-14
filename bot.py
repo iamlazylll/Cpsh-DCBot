a992014ae4917fca3736da8d6e224d5684034b65
IamLazyLLL <69131894+iamlazylll@users.noreply.github.com>
2024-03-14 05:50:29 +0000

    change

--- bot.py
+++ bot.py
@@ -1,13 +1,35 @@
 from dotenv import load_dotenv
 import os
 import discord
+import time
 load_dotenv()
-bot = discord.bot()
+bot = discord.Bot(intents = discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("你好 我是")
-    print(bot.user.name)
-    print(bot.user.id)
+    print(f"{bot.user}啟動！")
     print("------")
+
+@bot.slash_command(description="回覆訊息")
+async def hello(ctx):
+    await ctx.respond("hi")
+
+
+@bot.slash_command(description="現在時間")
+async def nowtime(ctx):
+    current_time = datetime.now()
+    current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    await ctx.respond(f"現在時間: `{current_time_str}`")
+
+@bot.slash_command(description="好欸")
+async def nowtime(ctx,st:str):
+    await ctx.respond(f"好欸, {st}")
+
+@bot.slash_command(description="現在時間")
+async def nowtime(ctx):
+    current_time = datetime.now()
+    current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    await ctx.respond(f"現在時間: '{current_time_str}'")
+
 bot.run(os.getenv("TOKEN"))
