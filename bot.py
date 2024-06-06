042b751e2224365cf7e53e346e178f6fd1479ae3
IamLazyLLL <eason4666@gmail.com>
2024-06-06 18:45:00 +0800

    uh...just changesLOL

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import discord
-import time
+import datetime
 load_dotenv()
 bot = discord.Bot(intents = discord.Intents.all())
 
@@ -18,18 +18,18 @@ async def hello(ctx):
 
 @bot.slash_command(description="現在時間")
 async def nowtime(ctx):
-    current_time = datetime.now()
+    current_time = datetime.datetime.now()
     current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
     await ctx.respond(f"現在時間: `{current_time_str}`")
 
-@bot.slash_command(description="好欸")
-async def nowtime(ctx,st:str):
-    await ctx.respond(f"好欸, {st}")
+# @bot.slash_command(description="好欸")
+# async def nowtime(ctx,st:str):
+#     await ctx.respond(f"好欸, {st}")
 
-@bot.slash_command(description="現在時間")
-async def nowtime(ctx):
-    current_time = datetime.now()
-    current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    await ctx.respond(f"現在時間: '{current_time_str}'")
-
-bot.run(os.getenv("TOKEN"))
+# @bot.slash_command(description="現在時間")
+# async def nowtime(ctx):
+#     current_time = datetime.now()
+#     current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+#     await ctx.respond(f"現在時間: '{current_time_str}'")
+TOKN = os.getenv("TOKEN")
+bot.run(TOKN)
